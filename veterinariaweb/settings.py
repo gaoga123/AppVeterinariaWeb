864754c7d2cd4eccce920650abd8471e0cdf39ed
gao <gaocampo2000@gmail.com>
2026-04-01 19:19:17 -0300

    Sol 5

--- veterinariaweb/settings.py
+++ veterinariaweb/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
